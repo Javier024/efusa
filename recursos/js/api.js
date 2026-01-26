a035223349bbea5cb2c6daa08efe037688a495b5
javier guzman <jguzmanmacea93@correo.unicordoba.edu.co>
2026-01-26 17:35:46 -0500

    feat: correccion

--- recursos/js/api.js
+++ recursos/js/api.js
@@ -1,21 +1,20 @@
-const API = '/api';
+// Archivo: recursos/js/api.js
 
 export async function getJugadores() {
-  return fetch(`${API}/jugadores`).then(r => r.json());
+  const res = await fetch('/api/jugadores');
+  return res.json();
 }
 
 export async function getPagos() {
-  return fetch(`${API}/pagos`).then(r => r.json());
+  const res = await fetch('/api/pagos');
+  return res.json();
 }
 
 export async function crearPago(data) {
-  return fetch(`${API}/pagos`, {
+  const res = await fetch('/api/pagos', {
     method: 'POST',
     headers: { 'Content-Type': 'application/json' },
     body: JSON.stringify(data)
-  }).then(r => r.json());
-}
-
-export async function getAlertas() {
-  return fetch(`${API}/alertas`).then(r => r.json());
+  });
+  return res.json();
 }
